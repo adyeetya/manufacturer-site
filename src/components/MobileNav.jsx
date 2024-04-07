7cec109047109f5d2ea3e2ae950da35d7383527c
adyeetya <2612adityasingh2000@gmail.com>
2024-04-07 14:33:39 +0530

    animation bug fix

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,33 +1,18 @@
 'use client'
 import React, { useState } from 'react'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 import Links from './Mobilemenu/Links'
 import Link from 'next/link'
 import Image from 'next/image'
 import ToggleButton from './Mobilemenu/ToggleButton'
 import { Lora } from 'next/font/google'
+
 const lora = Lora({
   weight: '400',
   subsets: ['latin'],
   display: 'swap',
 })
-const variants = {
-  animate: {
-    // clipPath: 'circle(1200px at 50px 50px)',
-    transition: {
-      ease: 'linear',
 
-      stiffness: 50,
-      duration: 0.4,
-      // staggerChildren: 0.1,
-    },
-
-    y: 0,
-  },
-  initial: {
-    y: -500,
-  },
-}
 const MobileNav = () => {
   const [open, setOpen] = useState(false)
 
@@ -39,20 +24,7 @@ const MobileNav = () => {
       <div className="flex justify-between w-full">
         <div className=" h-14 flex justify-start p-4 items-center">
           <Link href="/">
-            {/* <video
-              src="/videos/logo-manufacturing.mp4"
-              autoPlay
-              loop
-              muted
-              playsInline
-              className="h-14"
-            /> */}
             <div className="flex flex-row h-14 justify-center items-center">
-              {/* <h1
-                className={`${lora.className} tracking-tighter font-extrabold text-xl italic`}
-              >
-                <span className="italic">Modular Manufacturing</span>
-              </h1> */}
               <Image
                 src="/images/dik-logo.png"
                 alt=""
@@ -68,32 +40,22 @@ const MobileNav = () => {
         </div>
       </div>
 
-      {open && (
-        <motion.div
-          className="h-[calc(100vh-3rem)] overflow-y-auto scroll-smooth"
-          variants={variants}
-          initial="initial"
-          animate="animate"
-        >
-          <Links />
-        </motion.div>
-      )}
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            key="menu"
+            className="h-[calc(100vh-3rem)] overflow-y-auto scroll-smooth"
+            initial={{ y: -700 }}
+            animate={{ y: 0 }}
+            exit={{ y: -700 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Links />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </motion.div>
   )
 }
 
 export default MobileNav
-{
-  /* <motion.div
-          className="h-[calc(100vh-3rem)] overflow-y-auto scroll-smooth"
-          variants={variants}
-          initial="initial"
-          animate="animate"
-        >
-          <Links />
-        </motion.div>
-         <div className="h-[calc(100vh-3rem)] overflow-y-auto scroll-smooth">
-          <Links />
-        </div>
-        */
-}
